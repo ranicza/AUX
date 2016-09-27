895f5f80707f02173400e715040ee5c2fb6d891f
root <root@sandbox.hortonworks.com>
2016-09-27 15:34:15 +0000

    Draft

--- src/main/java/com/epam/bigdata/q3/task7/Producer.java
+++ src/main/java/com/epam/bigdata/q3/task7/Producer.java
@@ -2,16 +2,11 @@ package com.epam.bigdata.q3.task7;
 
 import com.google.common.io.Resources;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -19,16 +14,17 @@ import java.util.Properties;
 import java.util.stream.Stream;
 
 /**
- * This producer sends messages to topic "user-logs".
+ * This producer sends messages to topic "l-topic".
  * 
  * @author Maryna_Maroz
  *
  */
 public class Producer {
 
-	public static final String TOPIC = "t-topic";
+	public static final String TOPIC = "l-topic";
+	public static final String SEND = "SEND: ";
+	public static final String ISO = "ISO-8859-1";	
 	public static final String PARAMS_ERROR = "Usage: producer <file_path>";
-
 	
     public static void main(String[] args) throws IOException {
     	
@@ -43,10 +39,10 @@ public class Producer {
         try(Stream<java.nio.file.Path> paths = Files.walk(Paths.get(args[0]))) {
             paths.forEach(filePath -> {
                 if (Files.isRegularFile(filePath)) {
-                    try(Stream<String> lines = Files.lines(filePath, Charset.forName("ISO-8859-1"))) {
+                    try(Stream<String> lines = Files.lines(filePath, Charset.forName(ISO))) {
                         lines.forEach(line ->{
                             producer.send(new ProducerRecord<>(TOPIC, line));
-                            System.out.println("SEND: " + line);
+                            System.out.println(SEND + line);
                         });
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
--- src/main/java/com/epam/bigdata/q3/task7/TagsInterceptor.java
+++ src/main/java/com/epam/bigdata/q3/task7/TagsInterceptor.java
@@ -1,7 +1,6 @@
 package com.epam.bigdata.q3.task7;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.ArrayList;
@@ -23,59 +22,62 @@ import org.apache.commons.lang.StringUtils;
 public class TagsInterceptor implements Interceptor{
 	
 	private static final Logger log = LoggerFactory.getLogger(TagsInterceptor.class);
-    
-	//private static final String PATH = "/tmp/admin/hw7";
-//	private static final String TAB = "\\t";
-//	private static final String EVENT_DATE = "event_date";
-//	
 
-	
-    private static final String USER_TAGS_DICTIONARY = "/tmp/admin/hw7/user.profile.tags.us.tag.txt";
-    private static final String HDFS_ROOT_PATH = "hdfs://sandbox.hortonworks.com:8020";
-	private Map<String, String> tagsDictionary;
+    private static final String PATH_FILE = "/tmp/admin/hw7/user.profile.tags.us.tag.txt";
+    private static final String PATH = "hdfs://sandbox.hortonworks.com:8020";
+    private static final String TAB = "\\t";
+    private static final String HEADER_EVENT_DATE = "event_date";
+    private static final String HEADER_TAGS = "tags";
+    private static final String TRUE = "true";
+    private static final String FALSE ="false";
+    private static final String EMPTY =  "";
+    private static final String DELIMETER = "\t";
+    
+	private Map<String, String> tags;
 
 	@Override
     public void initialize() {
-        log.info("TagsInterceptor initialization is starting.");
-        prepareTagsDictionary();
+        getTags();
+        log.info("Interceptor initialized.");
     }
 
     @Override
     public Event intercept(Event event) {
         Map<String, String> headers = event.getHeaders();
+        String body = new String(event.getBody());
+        List<String> fields = null;
+        
+        if (StringUtils.isNotEmpty(body) && StringUtils.isNotBlank(body)) {
+        	fields = new ArrayList<>(Arrays.asList(body.split(TAB)));
 
-        String eventBodyStr = new String(event.getBody());
-        if (StringUtils.isNotEmpty(eventBodyStr) && StringUtils.isNotBlank(eventBodyStr)) {
-            List<String> params = new ArrayList<>(Arrays.asList(eventBodyStr.split("\\t")));
+        	// Get event date for partition
+            String dateEvent = fields.get(1).substring(0, 8);
+            headers.put(HEADER_EVENT_DATE, dateEvent);
 
-
-         //   String eventDate = params.get(1).substring(0, 8);
-          //  headers.put("event_date", eventDate);
-
-            String userTagsId = params.get(params.size() - 2);
-            String userTags = tagsDictionary.getOrDefault(userTagsId, "");
-            headers.put("tags_added", StringUtils.isNotBlank(userTags) ? "true" : "false");
-            params.add(userTags);
+            String tagsId = fields.get(fields.size() - 2);
+            String userTags = tags.getOrDefault(tagsId, EMPTY);
+            headers.put(HEADER_TAGS, StringUtils.isNotBlank(userTags) ? TRUE : FALSE);
+            fields.add(userTags);
 
             event.setHeaders(headers);
-            event.setBody(String.join("\t", params).getBytes());
+            event.setBody(String.join(DELIMETER, fields).getBytes());
         }
         return event;
     }
 
     @Override
     public java.util.List<Event> intercept(java.util.List<Event> events) {
-        List<Event> interceptedEvents = new ArrayList<Event>(events.size());
+        List<Event> intEvents = new ArrayList<Event>(events.size());
         for (Event event : events) {
-            Event interceptedEvent = intercept(event);
-            interceptedEvents.add(interceptedEvent);
+            Event intEvent = intercept(event);
+            intEvents.add(intEvent);
         }
-        return interceptedEvents;
+        return intEvents;
     }
 
     @Override
     public void close() {
-        log.info("TagsInterceptor closing.");
+        log.info("Interceptor closed.");
     }
 
     public static class Builder implements Interceptor.Builder {
@@ -90,22 +92,25 @@ public class TagsInterceptor implements Interceptor{
         }
     }
 
-    private void prepareTagsDictionary() {
+    private void getTags() {
+    	List<String> lines = new ArrayList<>();
+        String line;
+        
+    	Configuration config = new Configuration();       
+        Path path = new Path(PATH_FILE);
+        FileSystem fs = null;
+        BufferedReader br = null;
+        
         try {
-            Configuration config = new Configuration();
-            FileSystem fs = FileSystem.get(new URI(HDFS_ROOT_PATH), config);
-            Path path = new Path(USER_TAGS_DICTIONARY);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(path)));
+        	fs = FileSystem.get(new URI(PATH), config);
+            br = new BufferedReader(new InputStreamReader(fs.open(path)));
 
-            List<String> lines = new ArrayList<>();
-            String line;
             while ((line = br.readLine()) != null) {
-                log.info("Dictionary line " + line);
                 lines.add(line);
             }
-            this.tagsDictionary = lines.stream()
+            this.tags = lines.stream()
                     .skip(1)
-                    .map(s -> s.split("\\t"))
+                    .map(s -> s.split(TAB))
                     .collect(Collectors.toMap(
                             row -> row[0],
                             row -> row[1]
